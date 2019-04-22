f114a9be49c096e2ce3b916f75860f33c351c16a
Todd Tate <toddtatevw@gmail.com>
2019-04-22 13:27:11 -0600

    Initial File upload

---
+++ server.js
@@ -0,0 +1,31 @@
+//GETTING ALL THE NPM PACKAGES AS WELL AS SETTING MODELS AND PORT#
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var db = require("./models");
+var PORT = 3000;
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/macrumorsdb", { useNewUrlParser: true });
+
+//ROUTES
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+
