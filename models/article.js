068a85135f0028ef6db63f72dd2b1b4a26a6ea19
Todd Tate <toddtatedev@gmail.com>
2019-04-23 19:05:05 -0600

    Adding HBS Views
    having issues getting the hbs to work. Documentation is not good at all!

--- models/article.js
+++ models/article.js
@@ -18,13 +18,22 @@ var ArticleSchema = new Schema ({
         required: true
     },
 
+    saved: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+
     comments: [
         {
             type: Schema.Types.ObjectId,
             ref: "Comment"
         }
     ]
+
 });
+
+
 
 var Article = mongoose.model("Article", ArticleSchema);
 
--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = 3000;
+var PORT = 3001;
 // Initialize Express
 var app = express();
+var exphbs = require("express-handlebars");
 
 // Configure middleware
 
@@ -19,20 +20,32 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/macrumorsdb", { useNewUrlParser: true });
 
-
+app.get("/", function(req, res) {
+    res.render("home");
+})
 
 app.get("/scrape", function(req, res) {
     axios.get("http://www.macrumors.com").then(function(response) {
         var $ = cheerio.load(response.data);
 
-        $("")
+        $("div.article").each(function(i, element) {
+
+            var title = $(element).find("h2.title").text();
+            var urlLink = $(element).find("h2.title").find("a").attr("href");
+            var article = $(element).find("div.content_inner").text();
+
+        db.Article.create({ "title": title, "link": urlLink, "body": article})
     })
 })
 //ROUTES
-
+})
 
 
 
@@ -41,3 +54,7 @@ app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
   });
 
+
+
+ // Get data to display on the mian page
+ //Articals is equal to data and res.render.
