4e34437e6e65754c612c8bf82c392b5d0bef98d3
Todd Tate <toddtatedev@gmail.com>
2019-05-03 17:39:04 -0600

    saved.js
    Added modal to article comments button. Having trouble getting it to populate with data from DB...

--- public/saved.js
+++ public/saved.js
@@ -5,8 +5,27 @@ $.getJSON("/saved", function (data) {
             "<div class='card-header'>" +
             "<h3><a class='article-link' target='_blank' rel='noopener noreferrer' href='" + data[i].link + "'>" + data[i].title + "</a>   "
             + "<a class='btn btn-danger delete'>Delete From Saved</a>    "
-            + "   <a class='btn btn-info notes'>Article Comments</a>" +
+            + "   <button type='button' class='btn btn-info' data-toggle='modal' data-target='#commentModal' comments>Article Comments</a>" +
             "</h3>" + "</div>" +
+            "<div class='modal fade' id='commentModal' tabindex='-1' role='dialog' aria-labelledby='commentModalCenterTitle' aria-hidden='true'>" +
+            "<div class='modal-dialog modal-dialog-centered' role='document'>" +
+              "<div class='modal-content'>" +
+                "<div class='modal-header'>" +
+                  "<h5 class='modal-title' id='exampleModalLongTitle'>Comments</h5>" +
+                  "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>" +
+                    "<span aria-hidden='true'>&times;</span>" +
+                  "</button>" +
+                "</div>" +
+                "<div class='modal-body'>" +
+                  data[i].comments +
+                "</div>" +
+                "<div class='modal-footer'>" +
+                  "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>" +
+                  "<button type='button' class='btn btn-primary'>Save changes</button>" +
+                "</div>" +
+              "</div>" +
+            "</div>" +
+         "</div>" +
             "<div class='card-body'>" + data[i].body + "</div>" +
             "</div>"
         )
@@ -25,3 +44,34 @@ $(document).on("click", "a.delete", function () {
     })
     location.reload();
 });
+
+
+$(document).on("click", "a.comments", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).closest("div.card").attr("data-id");
+
+  
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from title input
+        title: $("#titleinput").val(),
+        // Value taken from note textarea
+        body: $("#bodyinput").val()
+      }
+    })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        // Empty the notes section
+        $("#notes").empty();
+      });
+  
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+  });
+  
